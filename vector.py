6e91911503b27a25226464420698ab5f4f2d6336
michaelian ennis <mennis@corvus.net>
2014-01-12 13:18:54 -0500

    add tests for normalize and magnitude against numpy fixed magnitude for x^2 not x^x etc.

--- vector.py
+++ vector.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from math import sqrt
 
 
@@ -10,9 +11,21 @@ class Vector(object):
 
     @property
     def magnitude(self):
-        return float(sqrt(self.x ** self.x + self.y ** self.y + self.z ** self.z))
+        """
+        Returns the vector magnitude or 'length'
+
+            $|x| = \sqrt{x^2 + y^2 + z^2}$
+
+        """
+        return float(sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2))
 
     def normalize(self):
+        """
+        Converts the vector into a unit vector whose magnitude is 1
+
+            $|x| = \sqrt{x^2 + y^2 + z^2} = 1$
+
+        """
         m = self.magnitude
         if m <= self.tolerance:
             m = 1
@@ -23,7 +36,7 @@ class Vector(object):
         for c in ['x', 'y', 'z']:
             component = getattr(self, c)
             if abs(component) < self.tolerance:
-                setattr(self, c, self.tolerance)
+                setattr(self, c, 0.0)
 
     def reverse(self):
         self.x = -self.x
@@ -57,7 +70,7 @@ class Vector(object):
 
     def __xor__(self, other):
         """
-        cross product
+        Returns the cross product of self and other
         """
         return Vector(
             self.y * other.x - self.z * other.y,
@@ -67,12 +80,12 @@ class Vector(object):
 
     def __mul__(self, other):
         """
-        dot product or scalar mult
+        Returns the dot product (or scalar mult if other is not a Vector) of self and other
         """
         if type(other) is not Vector:
             return Vector(self.x * other + self.y * other + self.z * other)
         else:
-            return Vector(self.x * other.x , self.y * other.y , self.z * other.z)
+            return Vector(self.x * other.x, self.y * other.y, self.z * other.z)
 
     def __repr__(self):
         r = str()
@@ -80,8 +93,29 @@ class Vector(object):
             r += "%s = %s\n" % (c, getattr(self, c))
         return r
 
+
 if __name__ == "__main__":
-    pass
+    import math
+    import numpy as np
+    from pprint import pprint as pp
 
-
-
+    print("magnitude")
+    a = np.array([4, 2, 7])
+    b = Vector(4, 2, 7)
+    n = np.sqrt(a.dot(a))
+    u = b.magnitude
+    print("np: %s" % n)
+    print("us: %s\t-> " % u, end='')
+    assert round(n) == round(u)
+    print("OK\n")
+    print("normalize")
+    n = a / np.linalg.norm(a)
+    b.normalize()
+    text = "np\t\t\tus:\nx={0:8f}\tx={1:8f}\ny={2:8f}\ty={3:8f}\nz={4:8f}\tz={5:8f}".format(n[0], b.x, n[1], b.y, n[2],
+                                                                                            b.z)
+    print(text, end='')
+    print("\t  -> ", end='')
+    assert round(n[0]) == round(b.x)
+    assert round(n[1]) == round(b.y)
+    assert round(n[2]) == round(b.z)
+    print("OK\n")
